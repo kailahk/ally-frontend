beb27321cc546a5eb82f25b73d07a415aae156a6
kailahk <kailahkorsh@me.com>
2023-07-21 16:59:43 -0700

    design: navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,17 +1,19 @@
 import './NavBar.css';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function NavBar({ user, handleLogout }) {
+    const url = window.location.href.split('/')[3];
     return (
         <nav className='navbar'>
             <h1 className='nav-logo'>ALLY</h1>
-            <Link className='dash-link' to='/'>Dashboard</Link>
-            <p className='welcome-user'>
-                Welcome, {user?.name}!
-            </p>
-            <Link className='logout-btn' to='' onClick={handleLogout}>
-                LOG OUT
-            </Link>
+            <div>
+                <Link className={url === '' ? 'dash-link active-nav' : 'dash-link'} to='/'>dashboard</Link>
+                <Link className={url === 'newfile' ? 'dash-link active-nav' : 'dash-link'} to='/newfile'>new file</Link>
+                <Link className={url === 'about' ? 'dash-link active-nav' : 'dash-link'} to='/about'>about</Link>
+                <Link className='logout-btn dash-link' to='' onClick={handleLogout}>
+                    LOG OUT
+                </Link>
+            </div>
         </nav>
     );
 }
