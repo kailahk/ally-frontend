c275baa8df1d09b4948ba3cb61c36b0b84db965e
kailahk <kailahkorsh@me.com>
2023-07-21 15:30:28 -0700

    design: landing page

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
 import './LandingPage.css'
 
 export default function LandingPage() {
+    const [friend, setFriend] = useState('friend');
     return (
-        <div className='landing-page-message'>
-            <h1 className='auth-page-title'>ALLY</h1>
-            <h3>Discover Ally, your personal relationship companion.</h3>
+        <div className='landing-page'>
+            <h1 className='title'>ALLY</h1>
+            <h3>BE THE BEST / {friend.toLocaleUpperCase()} / YOU CAN BE.</h3>
             <p>
-                Harness the power of AI-generated resources to strengthen your connections and become the ultimate support system for your loved ones. Sign up for free and elevate your relationships to new heights.
-            </p>
+                Harness the power of AI to get book, article, and podcast recommendations to give you insight into your / partner's / unique circumstances and help you do what you already do: support your loved ones.</p>
             <Link to="/authpage">Sign Up for Free</Link>
         </div>
     )
