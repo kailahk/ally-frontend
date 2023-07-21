3e9f17990f36a707b27d4a9871ad8c78f98accb7
kailahk <kailahkorsh@me.com>
2023-07-21 16:35:51 -0700

    feat: alternating text on landing page every 2 seconds

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './LandingPage.css'
 
 export default function LandingPage() {
     const [friend, setFriend] = useState('friend');
+    const friendOptions = ['sister', 'mother', 'brother', 'father', 'parent', 'partner', 'ally', 'grandparent', 'BFF', 'person', 'sibling', 'supporter']
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFriend(friendOptions[Math.floor(Math.random() * 11)]);
+        }, 2000);
+        return () => clearInterval(interval);
+    }, []);
     return (
         <div className='landing-page'>
             <h1 className='title'>ALLY</h1>
             <h3>BE THE BEST / {friend.toLocaleUpperCase()} / YOU CAN BE.</h3>
             <p>
-                Harness the power of AI to get book, article, and podcast recommendations to give you insight into your / partner's / unique circumstances and help you do what you already do: support your loved ones.</p>
+                Harness the power of AI to get book, article, and podcast recommendations to give you insight into your / person's / unique circumstances and help you do what you already do: support your loved ones.</p>
             <Link to="/authpage">Sign Up for Free</Link>
         </div>
     )
