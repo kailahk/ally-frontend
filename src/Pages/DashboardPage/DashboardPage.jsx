15edb285878d41c50c67565927b75eee735fcdd3
kailahk <kailahkorsh@me.com>
2023-07-22 17:41:48 -0700

    design: dashboard page styling

--- src/Pages/DashboardPage/DashboardPage.jsx
+++ src/Pages/DashboardPage/DashboardPage.jsx
@@ -8,9 +8,11 @@ export default function DashboardPage({ files, user }) {
     return (
         <div className='dashboard-page'>
             <h2>People</h2>
+            <img className='img-left' src="https://i.imgur.com/TQ9tQSC.png" alt="hand in the sunset" />
             <FileList user={user} />
+            <img className='img-right' src="https://i.imgur.com/BV16woX.png" alt="hand in the sunset" />
             <Link to='/newfile'>
-                <button className='new-file-btn'>Add Person</button>
+                <button className='new-file-btn'>new file</button>
             </Link>
         </div>
     )
