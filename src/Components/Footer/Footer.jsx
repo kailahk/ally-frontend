536f6e067ccb0862fb516f60506871d8b48dfebd
kailahk <kailahkorsh@me.com>
2023-07-24 11:53:42 -0700

    design: file details page, fix: footer links open to new window, design: about page scroll

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,18 @@
 import "./Footer.css"
+import { Link } from "react-router-dom"
 
 export default function Footer() {
     return (
         <div className="footer">
-            <a href="https://github.com/kailahk">
+            <Link target="blank" to="https://github.com/kailahk">
                 <img src="https://i.imgur.com/UqZKJZN.png" alt="github logo" />
-            </a>
-            <a href="https://kailahkorsh.com/">
+            </Link>
+            <Link target="blank" to="https://kailahkorsh.com/">
                 <img src="https://i.imgur.com/BaPmdWc.png" alt="kailah's website logo" />
-            </a>
-            <a href="https://www.linkedin.com/in/kailah-korsh/">
+            </Link>
+            <Link target="blank" to="https://www.linkedin.com/in/kailah-korsh/">
                 <img src="https://i.imgur.com/lt3NEt6.png" alt="linkedin logo" />
-            </a>
+            </Link>
         </div>
     )
 }
