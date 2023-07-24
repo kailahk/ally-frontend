25e53a0b7c7089c29cdd738c8c7b428ee9c8adb8
kailahk <kailahkorsh@me.com>
2023-07-23 17:56:50 -0700

    content: about page info

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -3,7 +3,15 @@ import './AboutPage.css'
 export default function AboutPage() {
     return (
         <div className="about-page">
-            <div className='app-info'></div>
+            <div className='about-info'>
+                <p>Welcome to Ally, your go-to platform for fostering meaningful relationships and becoming the best friend and family member you can be. Ally is more than just an app; it's a dedicated space where you can nurture your personal connections and offer genuine support to your loved ones.</p>
+                <p>Ally allows you to create a personalized file for each significant person in your life, capturing essential details about their experiences, whether they are celebrating joyous moments or navigating through challenging times. These thoughtful notes enable you to stay connected and demonstrate that you truly care.
+                </p>
+                <p>What sets Ally apart is its seamless integration of AI technology. By simply clicking the 'get AI-generated resources' button, you open a world of knowledge tailored to your loved one's unique circumstances. Our AI engine analyzes the information you provide and suggests curated recommendations, including books, articles, podcasts, and up-to-date research, all aimed at empowering you with insights to support your loved ones more effectively.
+                </p>
+                <p>Ally is more than just a tool; it's your ally in building deeper connections and fostering empathy. Join us on this journey of strengthening relationships and making a positive impact on the lives of those you care about most.
+                </p>
+            </div>
             <div className='rock-img'>
             </div>
         </div>
